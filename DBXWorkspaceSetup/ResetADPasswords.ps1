50b3725f5484cbecadc84f56e33e074e364e2ee0
Erik Zwiefel <erik.zwiefel@microsoft.com>
2018-08-17 16:34:15 -0500

    Adds management notebooks and reset password PS1

---
+++ DBXWorkspaceSetup/ResetADPasswords.ps1
@@ -0,0 +1,46 @@
+﻿$customDomain = "azureworkshops.net"
+$defaultPwd = "jhg342#9)6jk"
+
+# Set the number of groups and the number of users in each group
+$numUsers = 50
+
+# Connect to the AD tenant associated with the custom domain if you aren't already connected
+try 
+{
+    $domain = Get-AzureADDomain -Name $customDomain
+}
+catch [Microsoft.Open.AzureAD16.Client.ApiException],[Microsoft.Open.AzureAD16.PowerShell.GetDomain], [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException]
+{
+    Connect-AzureAD -TenantId $customDomain
+}
+catch 
+{
+    Write-Host "An error occurred at login that could not be resolved"
+    exit
+}
+
+# Set Password and ForceChangePasswordNextLogin parameters
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PasswordProfile.Password = $defaultPwd
+$PasswordProfile.ForceChangePasswordNextLogin = $false
+
+Write-Host ""
+Write-Host ""
+Write-Host "Resetting Passwords"
+Write-Host "==================="
+
+# Iterate users to reset passwords
+For ($userNum=1; $userNum -lt ($numUsers+1); $userNum++)
+{
+    $displayName = "User " + $userNum.ToString()
+    $userName = "user_" + $userNum.ToString()
+
+    $email = $userName + "@" + $customDomain
+
+    Write-Host "Resetting Password for user: $email"
+    $user = Get-AzureADUser -ObjectId $email
+    $objectId = $user.objectid
+
+    Set-AzureADUserPassword -ObjectId  $objectId -Password (ConvertTo-SecureString -String $defaultPwd -Force –AsPlainText)
+}
+Write-Host ""
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Azure Databricks workshop
 
-Please refer to reame.docs to get started with the workshop
+Please refer to README.docx to get started with the workshop
 
+Tools for setting up your own Databricks Workspace are available in the DBXWOrkspaceSetup folder - including DBC files (Databricks Archive Files) and Powershell Scripts. ___THESE TOOLS ARE ALPHA RELEASE.___
